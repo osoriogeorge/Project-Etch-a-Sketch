b02e6f1b58fda2b1a4142602bf1795c74ed69ae5
osoriogeorge <osoriovgeorge@gmail.com>
2025-01-23 10:55:46 +0100

    Implementing functions to create a grid, user choice, and styling the user interface

--- script.js
+++ script.js
@@ -1,5 +1,37 @@
-document.getElementById("#container");
+document.body.classList.add("container");
 
-let divs = document.createElements("divs");
+const btn = document.createElement("button");
+btn.textContent = "Click Me";
+document.body.appendChild(btn);
 
-document.appendChild(divs);
+function handleClick() {
+  const msg = window.prompt("Cuantos cuadrados por lados desea");
+  let userChoice = Number(msg);
+
+  if (isNaN(userChoice) || userChoice <= 0 || userChoice >= 100) {
+    return (msg = window.prompt("Cuantos cuadrados por lados desea"));
+  } else {
+    for (let i = 0; i < 256; i++) {
+      let div = document.createElement("div");
+      div.addEventListener("mouseover", () => {
+        div.style.backgroundColor = "darkRed";
+      });
+
+      container.appendChild(div);
+      console.log("new grid");
+    }
+  }
+}
+btn.addEventListener("click", handleClick);
+
+const container = document.getElementById("grid");
+
+for (let i = 0; i < 256; i++) {
+  let div = document.createElement("div");
+  div.addEventListener("mouseover", () => {
+    div.style.backgroundColor = "darkGrey";
+  });
+
+  container.appendChild(div);
+  console.log("Grid Generated");
+}
