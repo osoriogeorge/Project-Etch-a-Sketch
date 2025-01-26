1a00d490f6896ebf9cd3767029ca786dd9b144db
osoriogeorge <osoriovgeorge@gmail.com>
2025-01-26 12:10:23 +0100

    Add paragraph to display grid size
    - Added a new paragraph element to show the current size of the grid.
    - This helps users to know the dimensions of the grid they are working with.

--- script.js
+++ script.js
@@ -1,25 +1,50 @@
+let isMouseDown = false;
+
 function createSquare(size) {
   const square = document.createElement("div");
   square.style.width = `${size}px`;
   square.style.height = `${size}px`;
+
   square.addEventListener("mouseover", () => {
+    if (isMouseDown) {
+      square.style.backgroundColor = "black";
+    }
+  });
+
+  square.addEventListener("mousedown", () => {
     square.style.backgroundColor = "black";
   });
+
   return square;
 }
 
 function createGrid(size) {
   gridContainer.innerHTML = "";
+
   const squareSize = Math.floor(600 / size);
 
   for (let i = 0; i < size * size; i++) {
     const square = createSquare(squareSize);
     gridContainer.appendChild(square);
   }
+
+  gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  gridContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+  let paraInfoSize = document.getElementById("grid-size-info");
+
+  if (!paraInfoSize) {
+    paraInfoSize = document.createElement("p");
+    paraInfoSize.id = "grid-size-info";
+    document.body.appendChild(paraInfoSize);
+  }
+  paraInfoSize.textContent = `Grid size is: ${size}x${size}`;
 }
 
 function handleClick() {
-  const userResponse = parseInt(prompt("¿Cuántos cuadrados por lado deseas?"));
+  const userResponse = parseInt(
+    prompt("¿Cuántos cuadrados por lado deseas? (Máximo: 100)")
+  );
   if (isNaN(userResponse) || userResponse <= 0 || userResponse > 100) {
     alert("Por favor, ingresa un número válido entre 1 y 100.");
   } else {
@@ -28,10 +53,22 @@ function handleClick() {
 }
 
 const btn = document.createElement("button");
-btn.textContent = "Reiniciar Cuadrícula";
+btn.textContent = "Grid Size";
 btn.addEventListener("click", handleClick);
 document.body.insertBefore(btn, document.getElementById("grid"));
 
 const gridContainer = document.getElementById("grid");
+
+gridContainer.addEventListener("mousedown", () => {
+  isMouseDown = true;
+});
+
+gridContainer.addEventListener("mouseup", () => {
+  isMouseDown = false;
+});
+
+gridContainer.addEventListener("mouseleave", () => {
+  isMouseDown = false;
+});
 
 createGrid(16);
